3bed1359e25bf2ccd412bed25559b82a024874e9
forchinocafek-sys <forchino.cafe.k@gmail.com>
2026-08-31 21:17:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1536,6 +1536,7 @@ elif st.session_state["active_tab"] == "Сличительная":
                         if valid_notes:
                             # Формируем текст для всплывающей подсказки и клика
                             note_lines = "\n• " + "\n• ".join(valid_notes)
+                            safe_title = f"{note_lines}".replace('"', '&quot;').replace("'", '&apos;').replace("\n", "&#10;")
                             
                             js_comment = note_lines.replace("\\", "\\\\").replace("'", "\\'").replace('"', '&quot;').replace("\n", "\\n")
                             safe_stattya = r.replace("'", "\\'")
